ab4f0329ffecfddcd6f6b8d56ffe7587414ff131
Rodrigo Carvalho <rodrigocarvalhoshen@gmail.com>
2026-09-13 10:30:37 -0300

    -FIX some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import SunEffect from "@/components/ui/SunEffect";
+import SunEffect from "./components/ui/SunEffect";
 import { useEffect, useRef, useState } from "react";
 import "./styles/parallax.css";
 
