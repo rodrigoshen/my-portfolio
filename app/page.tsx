3fd67b110a25d508732ba24fb1730d239e47ff26
Rodrigo Carvalho <rodrigocarvalhoshen@gmail.com>
2026-09-21 22:44:40 -0300

    changing video playInline

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ function HomeContent() {
         />   
             <video
           className="absolute inset-0 w-screen h-screen object-cover" 
-          autoPlay loop muted>
+          autoPlay playsInline loop muted>
             <source src="/videos/background.mp4" type="video/mp4"/>
             Your Browser does not support video tag.
           </video>
